c57bfe85f6fde954a8d420f2994f76662e1d8e53
Thurinum <m.gagnon941@gmail.com>
2026-02-08 22:52:15 -0500

    feat: Add new NPC sprites

---
+++ Assets/GodBox/Visuals/ChooseRandomSprite.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace GodBox.Visuals
+{
+    public class ChooseRandomSprite : MonoBehaviour
+    {
+        public Sprite[] Sprites;
+
+        private void Start()
+        {
+            if (Sprites.Length == 0) return;
+
+            var spriteRenderer = GetComponent<SpriteRenderer>();
+            if (spriteRenderer)
+            {
+                int index = Random.Range(0, Sprites.Length);
+                spriteRenderer.sprite = Sprites[index];
+            }
+        }
+    }
+}
